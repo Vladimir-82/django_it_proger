792428515d25311f362f84fdb76ed79da31ec5ab
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-12-13 16:31:30 +0300

    it-proger-7 started

--- it_proger/main/views.py
+++ it_proger/main/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 # Create your views here.
 
+content = {
+    'key1': 1,
+    'values': ['Some', 'Hello', '123']
+}
+
+
+
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', content)
 
 def about(request):
     return render(request, 'main/about.html')
