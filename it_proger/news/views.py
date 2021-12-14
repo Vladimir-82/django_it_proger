f08b913612add620e62a8ea613a16025028857cf
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-12-14 10:04:36 +0300

    lesson 8 complete

--- it_proger/news/views.py
+++ it_proger/news/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Articles
 
 # Create your views here.
+
+
 def news_home(request):
-    return render(request, 'news/news_home.html')
+    news = Articles.objects.order_by('-date')  # objects.all() получаем все объекты из модели models Articles
+    return render(request, 'news/news_home.html', {'news': news})
+
+
