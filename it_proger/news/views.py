87331a179e04a7e401f31686c4285726f229a19e
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-12-16 13:51:34 +0300

    lesson 9 complete

--- it_proger/news/views.py
+++ it_proger/news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ActiclesForm
 
 # Create your views here.
 
@@ -7,5 +8,26 @@ from .models import Articles
 def news_home(request):
     news = Articles.objects.order_by('-date')  # objects.all() получаем все объекты из модели models Articles
     return render(request, 'news/news_home.html', {'news': news})
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = ActiclesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_home')
+        else:
+            error = 'Форма была неверной'
+
+
+    form = ActiclesForm()
+
+    data = {
+        'form': form,
+        'error': error
+    }
+
+    return render(request, 'news/create.html', data)
 
 
