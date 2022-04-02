efeafb909faaba5123e7f1e825eecf5517f6327f
vladimir <grun_gespenst@tut.by>
2022-04-02 15:11:05 +0300

    Diploy

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -28,9 +28,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import dj_database_url
 
 db_from_env = dj_database_url.config()
+
+CORS_ALLOW_ALL_ORIGINS = True
